fed8b3460271b521d759fc52cf53abe438d085d3
tfglab7 <gomezaguado94@gmail.com>
2018-02-13 20:27:29 +0100

    prueba para mandar cosas a odl

--- GlobalProgram.py
+++ GlobalProgram.py
@@ -1,4 +1,6 @@
 import os
+import requests
+from requests.auth import HTTPBasicAuth
 from classes import *
 
 def MenuIni():
@@ -10,9 +12,157 @@ def MenuIni():
 	print("5. Exit")
 
 def serviceConf():
+
 	print("You have chosen: Service configuration\n")
 	servicio = Service("30", "10", "5", "5", "833", "1", "internet")
 	servicio.showConfig()
+	
+	url="http://192.168.56.101:8181/restconf/config/opendaylight-inventory:nodes/node/openflow:346653127080/flow-node-inventory:table/"+servicio.idService
+	head = {'Content-type':'application/yang.data+json','Accept':'application/json, text/plain'}
+	payload='''{
+    "table": [
+        {
+            "id": "1",
+            "flow": [
+                {
+                    "id": "flow-up",
+                    "match": {
+                        "vlan-match": {
+                            "vlan-id": {
+                                "vlan-id-present": "false",
+                                "vlan-id": "833"
+                            }
+                        },
+                        "ipv4-source": "192.168.0.102/24"
+                    },
+                    "instructions": {
+                        "instruction": [
+                            {
+                                "order": "1",
+                                "apply-actions": {
+                                    "action": [
+                                        {
+                                            "order": "0",
+                                            "output-action": {
+                                                "output-node-connector": "NORMAL",
+                                                "max-length": "0"
+                                            }
+                                        }
+                                    ]
+                                }
+                            },
+                            {
+                                "order": "0",
+                                "meter": {
+                                    "meter-id": "11"
+                                }
+                            }
+                        ]
+                    },
+                    "flow-name": "flow-up",
+                    "installHw": "false",
+                    "barrier": "false",
+                    "strict": "false",
+                    "table_id": "1"
+                },
+                {
+                    "id": "flow-down",
+                    "match": {
+                        "vlan-match": {
+                            "vlan-id": {
+                                "vlan-id-present": "false",
+                                "vlan-id": "833"
+                            }
+                        },
+                        "ipv4-destination": "192.168.0.102/24"
+                    },
+                    "instructions": {
+                        "instruction": [
+                            {
+                                "order": "1",
+                                "apply-actions": {
+                                    "action": [
+                                        {
+                                            "order": "0",
+                                            "output-action": {
+                                                "output-node-connector": "NORMAL",
+                                                "max-length": "0"
+                                            }
+                                        }
+                                    ]
+                                }
+                            },
+                            {
+                                "order": "0",
+                                "meter": {
+                                    "meter-id": "12"
+                                }
+                            }
+                        ]
+                    },
+                    "flow-name": "flow-down",
+                    "installHw": "false",
+                    "barrier": "false",
+                    "strict": "false",
+                    "table_id": "1"
+                }
+            ]
+        }
+    ]
+}'''
+	meterup='''{
+    "meter": [
+        {
+            "flags": "meter-kbps",
+            "meter-id": "11",
+            "barrier": "true",
+            "meter-name": "meterup",
+            "container-name": "meterup",
+            "meter-band-headers": {
+                "meter-band-header": [
+                    {
+                        "band-id": "0",
+                        "meter-band-types": {
+                            "flags": "ofpmbt-drop"
+                        },
+                        "band-rate": "10000",
+                        "band-burst-size": "0",
+                        "drop-rate": "15000",
+                        "drop-burst-size": "0"
+                    }
+                ]
+            }
+        }
+    ]
+}'''
+	meterdown='''{
+    "meter": [
+        {
+            "flags": "meter-kbps",
+            "meter-id": "12",
+            "barrier": "true",
+            "meter-name": "meterdown",
+            "container-name": "meterdown",
+            "meter-band-headers": {
+                "meter-band-header": [
+                    {
+                        "band-id": "0",
+                        "meter-band-types": {
+                            "flags": "ofpmbt-drop"
+                        },
+                        "band-rate": "30000",
+                        "band-burst-size": "0",
+                        "drop-rate": "35000",
+                        "drop-burst-size": "0"
+                    }
+                ]
+            }
+        }
+    ]
+}'''
+	ret = requests.put(url,auth=HTTPBasicAuth('admin', 'admin'),headers=head,data=payload)
+
+
 
 def Main():
 
@@ -47,3 +197,4 @@ def Main():
 
 if __name__ == '__main__':
 	Main()
+
