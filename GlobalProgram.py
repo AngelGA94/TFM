46f693be8b4d14641d12c4a8b59291a09f40b77c
tfglab7 <gomezaguado94@gmail.com>
2018-02-14 20:12:36 +0100

    Main files have been updated

--- GlobalProgram.py
+++ GlobalProgram.py
@@ -3,6 +3,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 from classes import *
 
+
 def MenuIni():
 	print ("Welcome to the service management system.")
 	print("1. Service configuration")
@@ -11,158 +12,57 @@ def MenuIni():
 	print("4. Modify service attached to ONT")
 	print("5. Exit")
 
+def MenuService():
+	print("You have chosen: Service configuration\n")
+	print("1. Create new service")
+	print("2. Modify service")
+	print("3. Delete service")
+	print("4. Go back")
+
 def serviceConf():
 
-	print("You have chosen: Service configuration\n")
-	servicio = Service("30", "10", "5", "5", "833", "1", "internet")
-	servicio.showConfig()
-	
-	url="http://192.168.56.101:8181/restconf/config/opendaylight-inventory:nodes/node/openflow:346653127080/flow-node-inventory:table/"+servicio.idService
-	head = {'Content-type':'application/yang.data+json','Accept':'application/json, text/plain'}
-	payload='''{
-    "table": [
-        {
-            "id": "1",
-            "flow": [
-                {
-                    "id": "flow-up",
-                    "match": {
-                        "vlan-match": {
-                            "vlan-id": {
-                                "vlan-id-present": "false",
-                                "vlan-id": "833"
-                            }
-                        },
-                        "ipv4-source": "192.168.0.102/24"
-                    },
-                    "instructions": {
-                        "instruction": [
-                            {
-                                "order": "1",
-                                "apply-actions": {
-                                    "action": [
-                                        {
-                                            "order": "0",
-                                            "output-action": {
-                                                "output-node-connector": "NORMAL",
-                                                "max-length": "0"
-                                            }
-                                        }
-                                    ]
-                                }
-                            },
-                            {
-                                "order": "0",
-                                "meter": {
-                                    "meter-id": "11"
-                                }
-                            }
-                        ]
-                    },
-                    "flow-name": "flow-up",
-                    "installHw": "false",
-                    "barrier": "false",
-                    "strict": "false",
-                    "table_id": "1"
-                },
-                {
-                    "id": "flow-down",
-                    "match": {
-                        "vlan-match": {
-                            "vlan-id": {
-                                "vlan-id-present": "false",
-                                "vlan-id": "833"
-                            }
-                        },
-                        "ipv4-destination": "192.168.0.102/24"
-                    },
-                    "instructions": {
-                        "instruction": [
-                            {
-                                "order": "1",
-                                "apply-actions": {
-                                    "action": [
-                                        {
-                                            "order": "0",
-                                            "output-action": {
-                                                "output-node-connector": "NORMAL",
-                                                "max-length": "0"
-                                            }
-                                        }
-                                    ]
-                                }
-                            },
-                            {
-                                "order": "0",
-                                "meter": {
-                                    "meter-id": "12"
-                                }
-                            }
-                        ]
-                    },
-                    "flow-name": "flow-down",
-                    "installHw": "false",
-                    "barrier": "false",
-                    "strict": "false",
-                    "table_id": "1"
-                }
-            ]
-        }
-    ]
-}'''
-	meterup='''{
-    "meter": [
-        {
-            "flags": "meter-kbps",
-            "meter-id": "11",
-            "barrier": "true",
-            "meter-name": "meterup",
-            "container-name": "meterup",
-            "meter-band-headers": {
-                "meter-band-header": [
-                    {
-                        "band-id": "0",
-                        "meter-band-types": {
-                            "flags": "ofpmbt-drop"
-                        },
-                        "band-rate": "10000",
-                        "band-burst-size": "0",
-                        "drop-rate": "15000",
-                        "drop-burst-size": "0"
-                    }
-                ]
-            }
-        }
-    ]
-}'''
-	meterdown='''{
-    "meter": [
-        {
-            "flags": "meter-kbps",
-            "meter-id": "12",
-            "barrier": "true",
-            "meter-name": "meterdown",
-            "container-name": "meterdown",
-            "meter-band-headers": {
-                "meter-band-header": [
-                    {
-                        "band-id": "0",
-                        "meter-band-types": {
-                            "flags": "ofpmbt-drop"
-                        },
-                        "band-rate": "30000",
-                        "band-burst-size": "0",
-                        "drop-rate": "35000",
-                        "drop-burst-size": "0"
-                    }
-                ]
-            }
-        }
-    ]
-}'''
-	ret = requests.put(url,auth=HTTPBasicAuth('admin', 'admin'),headers=head,data=payload)
+	while True:
+		MenuService()
+		option=input("Please, select what do you want to do: ")
+
+		if int(option) == 1:
+			os.system('clear')
+			createService()
+			os.system('clear')
+		elif int(option) == 2:
+			os.system('clear')
+			input("opcion 2")
+			os.system('clear')
+		elif int(option) == 3:
+			os.system('clear')
+			deleteService()
+			os.system('clear')
+		elif int(option) == 4:
+			os.system('clear')
+			return
+		else:
+			os.system('clear')
+			input("Please, introduce a valid option!")
+			os.system('clear')
 
 
+def createService():
+	print("You have chosen: Create new service\n")
+	typeService = input("Type of service: ")
+	VLAN = input("VLAN: ")
+	downstream = input("Guaranteed downstream: ")
+	eDownstream = input("Excess downstream: ")
+	upstream = input("Guaranteed upstream: ")
+	eUpstream = input("Excess upstream: ")
+	servicio = Service(downstream, upstream, eDownstream, eUpstream, VLAN, typeService)
+	print("\n")
+	servicio.insertConfig()
+
+def deleteService():
+	print("You have chosen: Delete service\n")
+	Service.showServices()
+	id_service=input("Select the ID of the service which you want to delete: ")
+	Service.deleteConfig(id_service)
 
 def Main():
 
@@ -173,7 +73,6 @@ def Main():
 		if int(iniOption) == 1:
 			os.system('clear')
 			serviceConf()
-			input("opcion 1")
 			os.system('clear')
 		elif int(iniOption) == 2:
 			os.system('clear')
--- classes.py
+++ classes.py
@@ -1,24 +1,62 @@
+import mysql.connector
+
 class Service:
 
-	totDownstream = 0
-	totUpstream = 0
+	#totDownstream = 0
+	#totUpstream = 0
+	id_service=0
 
-	def __init__(self, gDownstream, gUpstream, excessDownstream, excessUpstream, VLAN, idService, typeService):
+	def __init__(self, gDownstream, gUpstream, excessDownstream, excessUpstream, VLAN, typeService):
 		self.gDownstream=gDownstream
 		self.gUpstream=gUpstream
 		self.excessDownstream=excessDownstream
 		self.excessUpstream=excessUpstream
 		self.VLAN=VLAN
-		self.idService=idService
+		#self.idService=idService
 		self.typeService=typeService
 
-		Service.totDownstream=Service.totDownstream+int(gDownstream)+int(excessDownstream)
-		Service.totUpstream=Service.totUpstream+int(gUpstream)+int(excessUpstream)
+		#Service.totDownstream=Service.totDownstream+int(gDownstream)+int(excessDownstream)
+		#Service.totUpstream=Service.totUpstream+int(gUpstream)+int(excessUpstream)
 
 	def showConfig(self):
+		print("VLAN: "+self.VLAN)
+		print("Type of service: "+ self.typeService)
 		print("Guaranteed downstream: " + self.gDownstream +" Mbps")
 		print("Excess downstream: " + self.excessDownstream +" Mbps")
 		print("Guaranteed upstream: " + self.gUpstream +" Mbps")
 		print("Excess upstream: " + self.excessUpstream +" Mbps")
-		print("VLAN: "+self.VLAN)
-		print("Type of service: "+ self.typeService)
+
+	def insertConfig(self):
+		cnx = mysql.connector.connect(user='root', password='tfg_2017',host='127.0.0.1',database='gponServices')
+		cursor = cnx.cursor()
+		query="insert into services (gDownstream, excessDownstream, gUpstream, excessUpstream, VLAN, typeService) values ('"+self.gDownstream+"', '"+self.excessDownstream+"', '"+self.gUpstream+"', '"+self.excessUpstream+"', '"+self.VLAN+"', '"+self.typeService+"')"
+		cursor.execute(query)
+		cnx.commit()
+
+		cursor.close()
+		cnx.close()
+
+	def showServices():
+		cnx = mysql.connector.connect(user='root', password='tfg_2017',host='127.0.0.1',database='gponServices')
+		cursor = cnx.cursor()
+		query="select * from services"
+		cursor.execute(query)
+		for (id_service, gDownstream, excessDownstream,gUpstream,excessUpstream,VLAN,typeService) in cursor:
+			print("{} {} {} {} {} {} {}".format(id_service, gDownstream, excessDownstream,gUpstream,excessUpstream,VLAN,typeService))
+
+
+		cursor.close()
+		cnx.close()
+
+	def deleteConfig(id_service):
+		cnx = mysql.connector.connect(user='root', password='tfg_2017',host='127.0.0.1',database='gponServices')
+		cursor = cnx.cursor()
+		query="delete from services where id_service='"+id_service+"'"
+		cursor.execute(query)
+		cnx.commit()
+
+		cursor.close()
+		cnx.close()
+
+
+
