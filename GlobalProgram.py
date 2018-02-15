70fa71950cfb6f900ce2f83664cbc92a6bd73d45
tfglab7 <gomezaguado94@gmail.com>
2018-02-15 19:59:55 +0100

    More changes

--- GlobalProgram.py
+++ GlobalProgram.py
@@ -14,10 +14,20 @@ def MenuIni():
 
 def MenuService():
 	print("You have chosen: Service configuration\n")
-	print("1. Create new service")
-	print("2. Modify service")
-	print("3. Delete service")
-	print("4. Go back")
+	print("1. List of configured services")
+	print("2. Create new service")
+	print("3. Modify service")
+	print("4. Delete service")
+	print("5. Go back")
+
+def MenuModify():
+	print("What parameter do you want to modify?\n")
+	print("1. Guaranteed downstream")
+	print("2. Excess downstream")
+	print("3. Guaranteed upstream")
+	print("4. Excess upstream")
+	print("5. VLAN")
+	print("6. Modify!\n")
 
 def serviceConf():
 
@@ -27,17 +37,22 @@ def serviceConf():
 
 		if int(option) == 1:
 			os.system('clear')
-			createService()
+			Service.showServices()
+			input("Press enter to continue...")
 			os.system('clear')
 		elif int(option) == 2:
 			os.system('clear')
-			input("opcion 2")
+			createService()
 			os.system('clear')
 		elif int(option) == 3:
 			os.system('clear')
-			deleteService()
+			modifyService()
 			os.system('clear')
 		elif int(option) == 4:
+			os.system('clear')
+			deleteService()
+			os.system('clear')
+		elif int(option) == 5:
 			os.system('clear')
 			return
 		else:
@@ -54,9 +69,52 @@ def createService():
 	eDownstream = input("Excess downstream: ")
 	upstream = input("Guaranteed upstream: ")
 	eUpstream = input("Excess upstream: ")
-	servicio = Service(downstream, upstream, eDownstream, eUpstream, VLAN, typeService)
+	service = Service(downstream, upstream, eDownstream, eUpstream, VLAN, typeService)
 	print("\n")
-	servicio.insertConfig()
+	service.insertConfig()
+
+def modifyService():
+	print("You have chosen: Modify service\n")
+	Service.showServices()
+	id_service=input("Select the ID of the service which you want to modify: ")
+	options=Service.getService(id_service)
+	service = Service(options[1], options[3], options[2], options[4], options[5], options[6])
+	os.system('clear')
+	while True:
+		print("New configuration:\n")
+		service.showConfig()
+		MenuModify()
+		option=input("Please, select what do you want to do: ")
+
+		if int(option) == 1:
+			os.system('clear')
+			service.gDownstream=input("Select new guaranteed downstream: ")
+			os.system('clear')
+		elif int(option) == 2:
+			os.system('clear')
+			service.excessDownstream=input("Select new excess downstream: ")
+			os.system('clear')
+		elif int(option) == 3:
+			os.system('clear')
+			service.gUpstream=input("Select new guaranteed upstream: ")
+			os.system('clear')
+		elif int(option) == 4:
+			os.system('clear')
+			service.excessUpstream=input("Select new excess upstream: ")
+			os.system('clear')
+		elif int(option) == 5:
+			os.system('clear')
+			service.VLAN=input("Select new VLAN: ")
+			os.system('clear')
+		elif int(option) == 6:
+			os.system('clear')
+			input("The service was modified. Press enter to continue...")
+			break
+		else:
+			os.system('clear')
+			input("Please, introduce a valid option!")
+			os.system('clear')
+	service.updateConfig(id_service)
 
 def deleteService():
 	print("You have chosen: Delete service\n")
@@ -65,6 +123,8 @@ def deleteService():
 	Service.deleteConfig(id_service)
 
 def Main():
+
+	os.system("clear")
 
 	while True:
 		MenuIni()
--- classes.py
+++ classes.py
@@ -1,10 +1,11 @@
 import mysql.connector
+from prettytable import PrettyTable
 
 class Service:
 
 	#totDownstream = 0
 	#totUpstream = 0
-	id_service=0
+	#id_service=0
 
 	def __init__(self, gDownstream, gUpstream, excessDownstream, excessUpstream, VLAN, typeService):
 		self.gDownstream=gDownstream
@@ -19,31 +20,45 @@ class Service:
 		#Service.totUpstream=Service.totUpstream+int(gUpstream)+int(excessUpstream)
 
 	def showConfig(self):
-		print("VLAN: "+self.VLAN)
-		print("Type of service: "+ self.typeService)
-		print("Guaranteed downstream: " + self.gDownstream +" Mbps")
-		print("Excess downstream: " + self.excessDownstream +" Mbps")
-		print("Guaranteed upstream: " + self.gUpstream +" Mbps")
-		print("Excess upstream: " + self.excessUpstream +" Mbps")
+		t = PrettyTable(['Guaranteed Downstream', 'Excess Downstream', 'Guaranteed Upstream', 'Excess Upstream', 'VLAN', 'Type of service'])
+		t.add_row([str(self.gDownstream)+" Mbps", str(self.excessDownstream)+" Mbps",str(self.gUpstream)+" Mbps",str(self.excessUpstream)+" Mbps",self.VLAN,self.typeService])
+		print(t)
 
 	def insertConfig(self):
 		cnx = mysql.connector.connect(user='root', password='tfg_2017',host='127.0.0.1',database='gponServices')
 		cursor = cnx.cursor()
-		query="insert into services (gDownstream, excessDownstream, gUpstream, excessUpstream, VLAN, typeService) values ('"+self.gDownstream+"', '"+self.excessDownstream+"', '"+self.gUpstream+"', '"+self.excessUpstream+"', '"+self.VLAN+"', '"+self.typeService+"')"
+		query="select sum(gDownstream) from services"
+		cursor.execute(query)
+		for i in cursor:
+			print(int(i)+2)
+		input()
+		# query="insert into services (gDownstream, excessDownstream, gUpstream, excessUpstream, VLAN, typeService) values ('"+str(self.gDownstream)+"', '"+str(self.excessDownstream)+"', '"+str(self.gUpstream)+"', '"+str(self.excessUpstream)+"', '"+str(self.VLAN)+"', '"+self.typeService+"')"
+		# cursor.execute(query)
+		# cnx.commit()
+
+		cursor.close()
+		cnx.close()
+
+	def updateConfig(self,id_service):
+		cnx = mysql.connector.connect(user='root', password='tfg_2017',host='127.0.0.1',database='gponServices')
+		cursor = cnx.cursor()
+		query="update services set gDownstream='"+str(self.gDownstream)+"', excessDownstream='"+str(self.excessDownstream)+"', gUpstream='"+str(self.gUpstream)+"', excessUpstream='"+str(self.excessUpstream)+"',VLAN='"+str(self.VLAN)+"' where id_service='"+id_service+"'"
 		cursor.execute(query)
 		cnx.commit()
 
 		cursor.close()
-		cnx.close()
+		cnx.close()		
 
 	def showServices():
 		cnx = mysql.connector.connect(user='root', password='tfg_2017',host='127.0.0.1',database='gponServices')
 		cursor = cnx.cursor()
 		query="select * from services"
 		cursor.execute(query)
+		t = PrettyTable(['ID', 'Guaranteed Downstream', 'Excess Downstream', 'Guaranteed Upstream', 'Excess Upstream', 'VLAN', 'Type of service'])
 		for (id_service, gDownstream, excessDownstream,gUpstream,excessUpstream,VLAN,typeService) in cursor:
-			print("{} {} {} {} {} {} {}".format(id_service, gDownstream, excessDownstream,gUpstream,excessUpstream,VLAN,typeService))
+			t.add_row([id_service, str(gDownstream)+" Mbps", str(excessDownstream)+" Mbps",str(gUpstream)+" Mbps",str(excessUpstream)+" Mbps",VLAN,typeService])
 
+		print(t)
 
 		cursor.close()
 		cnx.close()
@@ -57,6 +72,18 @@ class Service:
 
 		cursor.close()
 		cnx.close()
+
+	def getService(id_service):
+		cnx = mysql.connector.connect(user='root', password='tfg_2017',host='127.0.0.1',database='gponServices')
+		cursor = cnx.cursor()
+		query="select * from services where id_service='"+id_service+"'"
+		cursor.execute(query)
+		for (id_service,gDownstream, excessDownstream,gUpstream,excessUpstream,VLAN,typeService) in cursor:
+			options=[id_service,gDownstream, excessDownstream,gUpstream,excessUpstream,VLAN,typeService]
+		#service = Service(cursor.downstream, cursor.upstream, cursor.eDownstream, cursor.eUpstream, cursor.VLAN, cursor.typeService)
+		cursor.close()
+		cnx.close()
+		return options
 
 
 
